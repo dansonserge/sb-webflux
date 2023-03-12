53450e0aa5dc3c1dd5327323392ba6e27aca5e83
Serge Danson Ndekezi <sergedanson@gmail.com>
2023-03-12 22:15:51 +0200

    initializing the webflux project with postgresql

---
+++ src/main/java/com/sergedanson/spwebflux/SpWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.sergedanson.spwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpWebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpWebfluxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sergedanson/spwebflux/SpWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergedanson.spwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpWebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

482c6ee3eca6faca3b9d24926d75c2a740f65aa7
Serge Danson Ndekezi <sergedanson@gmail.com>
2023-03-13 02:25:42 +0200

    added flyway db  migration

---
+++ src/main/resources/db/migration/V1__create_user_entity_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS user_entity (
+                                           id SERIAL PRIMARY KEY,
+                                           firstName VARCHAR(255) NOT NULL,
+                                           lastName VARCHAR(255) NOT NULL,
+                                           email VARCHAR(255) NOT NULL UNIQUE,
+                                           password VARCHAR(255) NOT NULL,
+                                           is_active BOOLEAN NOT NULL DEFAULT false
+);
